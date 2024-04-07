c38d8f701e0c59d2804f2dac437a407e08fb81c4
Maxim Kim <kimmaxim69@gmail.com>
2024-04-07 20:25:02 +0300

    Добавил файлы по ДЗ №7

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+import pytest
+
+
+from zipfile import ZipFile
+
+
+CURRENT_FILE = os.path.abspath(__file__)
+PROJECT_DIR = os.path.dirname(os.path.dirname(CURRENT_FILE ))
+TMP_DIR = os.path.join(PROJECT_DIR, 'temp')
+RESOURCE_DIR = os.path.join(PROJECT_DIR, 'resource')
+ARCHIVE = os.path.join(RESOURCE_DIR, 'archive.zip')
+
+
+@pytest.fixture(scope="session", autouse=True)
+def archive_files():
+    if not os.path.exists(RESOURCE_DIR):
+        os.mkdir(RESOURCE_DIR)
+
+    with ZipFile(ARCHIVE, "w") as zip_file:
+        for file in os.listdir(TMP_DIR):
+            add_file = os.path.join(TMP_DIR, file)
+            zip_file.write(add_file, os.path.basename(add_file))
+    yield
+    shutil.rmtree(RESOURCE_DIR)
+    
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,107 +1,37 @@
-from datetime import time
+import os
+
+from zipfile import ZipFile
+from pypdf import PdfReader
+from openpyxl import load_workbook
+from tests.conftest import ARCHIVE
 
 
-def test_dark_theme_by_time():
-    """
-    Протестируйте правильность переключения темной темы на сайте в зависимости от времени
-    """
-    current_time = time(hour=23)
-    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-    if 6 <= current_time.hour < 22:
-        is_dark_theme = False
-    else:
-        is_dark_theme = True
-    assert is_dark_theme is True
+def test_pdf():
+    with ZipFile(ARCHIVE, 'r') as zip_file:
+        with zip_file.open('PDF.pdf', 'r') as pdf_file:
+            reader = PdfReader(pdf_file)
+            text_found = False
+            for page_num, page in enumerate(reader.pages, 1):
+                page_text = page.extract_text()
+                if 'Roman Luzin' in page_text:
+                    text_found = True
+                    break
+                else:
+                    print(f"Text not found in page {page_num}: {page_text}")
+            assert text_found, "Text 'Roman Luzin' not found in PDF"
 
 
-def test_dark_theme_by_time_and_user_choice():
-    """
-    Протестируйте правильность переключения темной темы на сайте
-    в зависимости от времени и выбора пользователя
-    dark_theme_enabled_by_user = True - Темная тема включена
-    dark_theme_enabled_by_user = False - Темная тема выключена
-    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
-    """
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
-    # TODO переключите темную тему в зависимости от времени суток,
-    #  но учтите что темная тема может быть включена вручную
-
-    if dark_theme_enabled_by_user is True:
-        is_dark_theme = True
-    elif dark_theme_enabled_by_user is False:
-        is_dark_theme = False
-    elif 6 <= current_time.hour < 22 and dark_theme_enabled_by_user is None:
-        is_dark_theme = False
-    else:
-        is_dark_theme = True
-    assert is_dark_theme is True
+def test_xlsx():
+    with ZipFile(ARCHIVE, 'r') as zip_file:
+        with zip_file.open('Excel.xlsx', 'r') as xlsx_file:
+            workbook = load_workbook(xlsx_file)
+            sheet = workbook.active
+            assert sheet['B9'].value == 'Тест фавикона', "Text 'Тест фавикона' not found in cell B9"
 
 
-def test_find_suitable_user():
-    """
-    Найдите нужного пользователя по условиям в списке пользователей
-    """
-    users = [
-        {"name": "Oleg", "age": 32},
-        {"name": "Sergey", "age": 24},
-        {"name": "Stanislav", "age": 15},
-        {"name": "Olga", "age": 45},
-        {"name": "Maria", "age": 18},
-    ]
-
-    # TODO найдите пользователя с именем "Olga"
-    for user in users:
-        if user['name'] == "Olga":
-            suitable_users = user
-    assert suitable_users == {"name": "Olga", "age": 45}
-
-    # TODO найдите всех пользователей младше 20 лет
-    suitable_users = []
-    for user in users:
-        if user['age'] < 20:
-            suitable_users.append(user)
-    assert suitable_users == [
-        {"name": "Stanislav", "age": 15},
-        {"name": "Maria", "age": 18},
-    ]
-
-
-# Сделайте функцию, которая будет печатать
-# читаемое имя переданной ей функции и значений аргументов.
-# Вызовите ее внутри функций, описанных ниже
-# Подсказка: Имя функции можно получить с помощью func.__name__
-# Например, вызов следующей функции должен преобразовать имя функции
-# в более читаемый вариант (заменить символ подчеркивания на пробел,
-# сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
-# >>> open_browser(browser_name="Chrome")
-# "Open Browser [Chrome]"
-
-
-def test_readable_function():
-    open_browser(browser_name="Chrome")
-    go_to_companyname_homepage(page_url="https://companyname.com")
-    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
-
-
-def print_name(foo, *args):
-    res_name = foo.__name__.replace('_', ' ').title()
-    res_args = ', '.join(args)
-    res = f'{res_name} [{res_args}]'
-    print(res)
-    return res
-
-
-def open_browser(browser_name):
-    actual_result = print_name(open_browser, browser_name)
-    assert actual_result == "Open Browser [Chrome]"
-
-
-def go_to_companyname_homepage(page_url):
-    actual_result = print_name(go_to_companyname_homepage, page_url)
-    assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
-
-
-def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = print_name(find_registration_button_on_login_page, page_url, button_text)
-    assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+def test_csv():
+    file_size_bytes = os.path.getsize(ARCHIVE)
+    file_size_mb = file_size_bytes / (1024 * 1024)
+    rounded_file_size_mb = round(file_size_mb, 2)
+    expected_size_mb = 1.55
+    assert rounded_file_size_mb == expected_size_mb, f"CSV file size is not approximately 1.55 MB (rounded to nearest MB)"
