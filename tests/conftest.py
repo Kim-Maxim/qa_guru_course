ccd800bc80ee3dc933f7caa14d1cb639e302fe7d
Maxim Kim <kimmaxim69@gmail.com>
2024-04-17 10:50:32 +0300

    Улучшил фикстуру для прогона автотестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,37 @@
 import pytest
 
-from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_height = 1920
-    browser.config.window_width = 1080
-    # browser.config.type_by_js = True
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    chrome_options.page_load_strategy = 'eager'
-    browser.config.driver_options = chrome_options
-    yield
+def setup_browser(request):
+    Browser.config.base_url = 'https://demoqa.com'
+    Browser.config.window_height = 1920
+    Browser.config.window_width = 1080
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
--- tests/test_form_demoqa.py
+++ tests/test_form_demoqa.py
@@ -1,38 +1,44 @@
 import os
+import allure
+
 
 from selene import browser, have, be
 
 
+@allure.title("Successful fill form")
 def test_fill_all_fields():
-    browser.open('/automation-practice-form')
-    browser.element('#firstName').should(be.blank).type('Ivan')
-    browser.element('#lastName').should(be.blank).type('Ivanov')
-    browser.element('#userEmail').type('ivan.ivanov@gmail.com')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type('8005553535')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select').click().element('option[value="1991"]').click()
-    browser.element('.react-datepicker__month-select').click().element('option[value="11"]').click()
-    browser.element('.react-datepicker__day--026').click()
-    browser.element('#subjectsInput').type('Maths').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('tests/image.png'))
-    browser.element('#currentAddress').type('Russia')
-    browser.element('#react-select-3-input').type('hary').press_enter()
-    browser.element('#react-select-4-input').type('karn').press_enter()
-    browser.element('#submit').click()
-    browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
-    browser.element('.table').all('td').even.should(
-        have.exact_texts(
-            'Ivan Ivanov',
-            'ivan.ivanov@gmail.com',
-            'Male',
-            '8005553535',
-            '26 December,1991',
-            'Maths',
-            'Sports',
-            'image.png',
-            'Russia',
-            'Haryana Karnal'
+    with allure.step("Open registration form"): 
+        browser.open('/automation-practice-form')
+    with allure.step("Fill form"):
+        browser.element('#firstName').should(be.blank).type('Ivan')
+        browser.element('#lastName').should(be.blank).type('Ivanov')
+        browser.element('#userEmail').type('ivan.ivanov@gmail.com')
+        browser.element('[for="gender-radio-1"]').click()
+        browser.element('#userNumber').type('8005553535')
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__year-select').click().element('option[value="1991"]').click()
+        browser.element('.react-datepicker__month-select').click().element('option[value="11"]').click()
+        browser.element('.react-datepicker__day--026').click()
+        browser.element('#subjectsInput').type('Maths').press_enter()
+        browser.element('[for="hobbies-checkbox-1"]').click()
+        browser.element('#uploadPicture').send_keys(os.path.abspath('tests/image.png'))
+        browser.element('#currentAddress').type('Russia')
+        browser.element('#react-select-3-input').type('hary').press_enter()
+        browser.element('#react-select-4-input').type('karn').press_enter()
+        browser.element('#submit').click()
+        browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
+    with allure.step("Check form results"):
+        browser.element('.table').all('td').even.should(
+            have.exact_texts(
+                'Ivan Ivanov',
+                'ivan.ivanov@gmail.com',
+                'Male',
+                '8005553535',
+                '26 December,1991',
+                'Maths',
+                'Sports',
+                'image.png',
+                'Russia',
+                'Haryana Karnal'
+            )
         )
-    )
